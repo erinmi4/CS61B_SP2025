11679ea0cb693c9db3e26bbb44118dbd8c487151
mika <mikasa2511592510@gmail.com>
2025-05-18 23:56:57 +0800

    开始project1a

---
+++ HW/regular01/CS61B.java
@@ -0,0 +1,12 @@
+/** Haha get it...because CS61B is a class...at a public university...I'll see myself out */
+public class CS61B {
+// Variables (part a)
+// Constructor (part b)
+// Methods (part c)
+/** Makes every CS61BStudent enrolled in this semester of the course watch lecture. Returns the
+number of students who actually watched lecture. */
+/** Takes in a new university name newUniversity and changes the university
+for all semesters of CS61B to newUniversity. */
+// Expansion (part d)
+/** Expands the course to the given capacity. */
+}
