0dadb23da35a64511a35048dbf66d173c3fe4367
mika <mikasa2511592510@gmail.com>
2025-05-18 08:08:34 +0800

    Add university field to CS61BStudent class lab2

--- HW/regular01/CS61BStudent.java
+++ HW/regular01/CS61BStudent.java
@@ -1,16 +1,22 @@
-public class CS61BStudent { // Class Declaration
-public int idNumber; // Instance Variables
-public int grade;
-public static String instructor = "Hug"; // Class (Static) Variables
-public CS61BStudent(int id) { // Constructor
-this.idNumber = id;
-this.grade = 100;
-}
-public boolean watchLecture() { // Instance Method
-// Returns whether the student actually watched the lecture
-...
-}
-public static String getInstructor() { // Static Method
-...
-}
+public class CS61BStudent {
+  // Class Declaration
+  public int idNumber; // Instance Variables
+  public int grade;
+  public static String instructor = "Hug"; // Class (Static) Variables
+  public static String university = "UC Berkeley"
+
+  public CS61BStudent(int id) {
+    // Constructor
+  this.idNumber = id;
+  this.grade = 100;
+  }
+
+  public boolean watchLecture() {
+    // Instance Method
+  // Returns whether the student actually watched the lecture
+
+  }
+  public static String getInstructor() { // Static Method
+  ...
+  }
 }
